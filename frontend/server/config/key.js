3703e3b35e856227d0c530cb5e3a55986e67d0b9
Yoona jeong <yoonalove77@naver.com>
2022-06-02 17:43:07 +0900

    logic

--- frontend/server/config/key.js
+++ frontend/server/config/key.js
@@ -1,4 +1,5 @@
-const CLIENT_ID = '146414e0f2cf5ef05dee863aae51615a';
-const REDIRECT_URI = 'http://localhost:8888/oauth/login/kakao';
-
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
