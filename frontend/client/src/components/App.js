2a60bb8f52017d36404fda943fe4ce2e28371271
Yoona jeong <yoonalove77@naver.com>
2022-06-28 15:45:08 +0900

    [feat] 브라우저 감지를 통한 제한 설정 및 디자인 수정

--- frontend/client/src/components/App.js
+++ frontend/client/src/components/App.js
@@ -1,81 +1,159 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
+import { isBrowser, BrowserView, MoblieView } from "react-device-detect";
+
 import Auth from "../hoc/auth";
 // pages for this product
 import LandingPage from "./views/LandingPage/LandingPage.js";
 
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
-import NotificationPage from './views/NotificationPage/NotificationPage';
+import NotificationPage from "./views/NotificationPage/NotificationPage";
 
 // import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
-import AuthRedirectHandler from './views/LoginPage/KakaoLogin/AuthRedirectHandler';
-import KakaoLoginPage from './views/LoginPage/KakaoLogin/KakaoLoginPage';
+import AuthRedirectHandler from "./views/LoginPage/KakaoLogin/AuthRedirectHandler";
+import KakaoLoginPage from "./views/LoginPage/KakaoLogin/KakaoLoginPage";
 
-import MyPage from './views/MyPage/MyPage';
-import MyStoryList from './views/MyPage/MyStoryList/MyStoryList';
+import MyPage from "./views/MyPage/MyPage";
+import MyStoryList from "./views/MyPage/MyStoryList/MyStoryList";
 
-import ArtMeetingPage from './views/ArtMeetingPage/ArtMeetingPage'
-import DailyMeetingPage from './views/DailyMeetingPage/DailyMeetingPage'
-import ExerciseMeetingPage from './views/ExerciseMeetingPage/ExerciseMeetingPage'
-import FoodMeetingPage from './views/FoodMeetingPage/FoodMeetingPage'
-import MusicMeetingPage from './views/MusicMeetingPage/MusicMeetingPage'
-import TripMeetingPage from './views/TripMeetingPage/TripMeetingPage'
+import ArtMeetingPage from "./views/ArtMeetingPage/ArtMeetingPage";
+import DailyMeetingPage from "./views/DailyMeetingPage/DailyMeetingPage";
+import ExerciseMeetingPage from "./views/ExerciseMeetingPage/ExerciseMeetingPage";
+import FoodMeetingPage from "./views/FoodMeetingPage/FoodMeetingPage";
+import MusicMeetingPage from "./views/MusicMeetingPage/MusicMeetingPage";
+import TripMeetingPage from "./views/TripMeetingPage/TripMeetingPage";
 
-import HobbyStoryPage from './views/HobbyStoryPage/HobbyStoryPage'
-import MakeMeetingPage from './views/MakeMeetingPage/MakeMeetingPage'
+import HobbyStoryPage from "./views/HobbyStoryPage/HobbyStoryPage";
+import MakeMeetingPage from "./views/MakeMeetingPage/MakeMeetingPage";
 
-import UploadExercise from './views/MakeMeetingPage/UploadMeetingPage/UploadExercise'
-import UploadArt from './views/MakeMeetingPage/UploadMeetingPage/UploadArt';
-import UploadDaily from './views/MakeMeetingPage/UploadMeetingPage/UploadDaily';
-import UploadFood from './views/MakeMeetingPage/UploadMeetingPage/UploadFood';
-import UploadMusic from './views/MakeMeetingPage/UploadMeetingPage/UploadMusic';
-import UploadTrip from './views/MakeMeetingPage/UploadMeetingPage/UploadTrip';
-
+import UploadExercise from "./views/MakeMeetingPage/UploadMeetingPage/UploadExercise";
+import UploadArt from "./views/MakeMeetingPage/UploadMeetingPage/UploadArt";
+import UploadDaily from "./views/MakeMeetingPage/UploadMeetingPage/UploadDaily";
+import UploadFood from "./views/MakeMeetingPage/UploadMeetingPage/UploadFood";
+import UploadMusic from "./views/MakeMeetingPage/UploadMeetingPage/UploadMusic";
+import UploadTrip from "./views/MakeMeetingPage/UploadMeetingPage/UploadTrip";
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
 //false  logged in user can't go inside
 
 function App() {
+  if (isBrowser) {
+    return (
+      <div style={{ width: "100%" }}>
+        <div style={{ margin: "3rem auto" }}>
+          <h1 style={{ fontSize: "60px" }}>모바일 환경으로 접속해주세요</h1>
+        </div>
+      </div>
+    );
+  }
   return (
-    <Suspense fallback={(<div>Loading...</div>)}>
+    <Suspense fallback={<div>Loading...</div>}>
       <NavBar />
-      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+      <div style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }}>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
 
           <Route exact path="/login" component={Auth(KakaoLoginPage, false)} />
-          <Route exact path="/oauth/login/kakao" component={Auth(AuthRedirectHandler, false)} />
+          <Route
+            exact
+            path="/oauth/login/kakao"
+            component={Auth(AuthRedirectHandler, false)}
+          />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
 
-          <Route exact path="/notification" component={Auth(NotificationPage, null)} />
+          <Route
+            exact
+            path="/notification"
+            component={Auth(NotificationPage, null)}
+          />
 
           {/* 마이페이지 */}
           {/* 나중에 null->true로 변경 + userId가 주소값에 할당되게 */}
           <Route exact path="/mypage" component={Auth(MyPage, null)} />
-          <Route exact path="/mypage/mystory" component={Auth(MyStoryList, null)} />
+          <Route
+            exact
+            path="/mypage/mystory"
+            component={Auth(MyStoryList, null)}
+          />
 
           {/* 취미 커뮤니티 */}
-          <Route exact path="/artMeeting" component={Auth(ArtMeetingPage, null)} />
-          <Route exact path="/dailyMeeting" component={Auth(DailyMeetingPage, null)} />
-          <Route exact path="/exerciseMeeting" component={Auth(ExerciseMeetingPage, null)} />
-          <Route exact path="/foodMeeting" component={Auth(FoodMeetingPage, null)} />
-          <Route exact path="/musicMeeting" component={Auth(MusicMeetingPage, null)} />
-          <Route exact path="/tripMeeting" component={Auth(TripMeetingPage, null)} />
+          <Route
+            exact
+            path="/artMeeting"
+            component={Auth(ArtMeetingPage, null)}
+          />
+          <Route
+            exact
+            path="/dailyMeeting"
+            component={Auth(DailyMeetingPage, null)}
+          />
+          <Route
+            exact
+            path="/exerciseMeeting"
+            component={Auth(ExerciseMeetingPage, null)}
+          />
+          <Route
+            exact
+            path="/foodMeeting"
+            component={Auth(FoodMeetingPage, null)}
+          />
+          <Route
+            exact
+            path="/musicMeeting"
+            component={Auth(MusicMeetingPage, null)}
+          />
+          <Route
+            exact
+            path="/tripMeeting"
+            component={Auth(TripMeetingPage, null)}
+          />
 
           {/* 모임 초대*/}
-          <Route exact path="/makeMeeting" component={Auth(MakeMeetingPage, null)} />
-          <Route exact path="/makeMeeting/exercise" component={Auth(UploadExercise, null)} />
-          <Route exact path="/makeMeeting/art" component={Auth(UploadArt, null)} />
-          <Route exact path="/makeMeeting/daily" component={Auth(UploadDaily, null)} />
-          <Route exact path="/makeMeeting/food" component={Auth(UploadFood, null)} />
-          <Route exact path="/makeMeeting/music" component={Auth(UploadMusic, null)} />
-          <Route exact path="/makeMeeting/trip" component={Auth(UploadTrip, null)} />
+          <Route
+            exact
+            path="/makeMeeting"
+            component={Auth(MakeMeetingPage, null)}
+          />
+          <Route
+            exact
+            path="/makeMeeting/exercise"
+            component={Auth(UploadExercise, null)}
+          />
+          <Route
+            exact
+            path="/makeMeeting/art"
+            component={Auth(UploadArt, null)}
+          />
+          <Route
+            exact
+            path="/makeMeeting/daily"
+            component={Auth(UploadDaily, null)}
+          />
+          <Route
+            exact
+            path="/makeMeeting/food"
+            component={Auth(UploadFood, null)}
+          />
+          <Route
+            exact
+            path="/makeMeeting/music"
+            component={Auth(UploadMusic, null)}
+          />
+          <Route
+            exact
+            path="/makeMeeting/trip"
+            component={Auth(UploadTrip, null)}
+          />
 
           {/* 스토리 */}
-          <Route exact path="/hobbyStory" component={Auth(HobbyStoryPage, null)} />
+          <Route
+            exact
+            path="/hobbyStory"
+            component={Auth(HobbyStoryPage, null)}
+          />
         </Switch>
       </div>
       <Footer />
--- frontend/client/src/components/views/LandingPage/LandingPage.js
+++ frontend/client/src/components/views/LandingPage/LandingPage.js
@@ -1,196 +1,244 @@
-import React from 'react'
+import React from "react";
 // import { FaCode } from "react-icons/fa";
-import {Button, Input} from 'antd'
+import { Button, Input } from "antd";
+import { BrowerView, MoblieView } from "react-device-detect";
 
-import CharacterImg from './img/character.png'
-import ArtImg from './img/art.png'
-import DailyImg from './img/daily.png'
-import ExerciseImg from './img/exercise.png'
-import FoodImg from './img/food.png'
-import HobbyStoryImg from './img/hobbyStory.png'
-import MusicImg from './img/music.png'
-import TripImg from './img/trip.png'
+import CharacterImg from "./img/character.png";
+import ArtImg from "./img/art.png";
+import DailyImg from "./img/daily.png";
+import ExerciseImg from "./img/exercise.png";
+import FoodImg from "./img/food.png";
+import HobbyStoryImg from "./img/hobbyStory.png";
+import MusicImg from "./img/music.png";
+import TripImg from "./img/trip.png";
 
 function LandingPage() {
+  const { Search } = Input;
 
-    const {Search} = Input
+  const onSearch = (value) => console.log(value);
 
-    const onSearch = (value) => console.log(value);
+  const hobbyBoxStyle = {
+    width: "108px",
+    height: "108px",
+    border: "solid 0.8px #A5A5A5",
+    borderRadius: "15px",
+    display: "inline-block",
+    margin: "7px",
+    padding: "10px",
+  };
 
-    const hobbyBoxStyle = {
-        width: '108px',
-        height: '108px',
-        border: 'solid 0.8px #A5A5A5',
-        borderRadius: '15px',
-        display: 'inline-block',
-        margin: '7px',
-        padding: '10px'
-    }
+  const hobbyImageStyle = {
+    width: "62px",
+    height: "62px",
+  };
 
-    const hobbyImageStyle = {
-        width: '62px',
-        height: '62px'
-    }
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          margin: "3rem auto",
+        }}
+      >
+        <Search
+          placeholder="찾으시는 취미를 검색해보세요!"
+          onSearch={onSearch}
+          style={{
+            textAlign: "center",
+            width: "90%",
+          }}
+          // enterButton
+          size="large"
+        />
 
-    return (
+        <h4
+          style={{
+            textAlign: "left",
+            color: "#787878",
+            paddingTop: "30px",
+            marginLeft: "27px",
+          }}
+        >
+          취미 카테고리
+        </h4>
+
+        <a
+          href="/exerciseMeeting"
+          style={{ textDecoration: "none", color: "#000" }}
+        >
+          <div style={hobbyBoxStyle}>
+            운동
+            <img src={ExerciseImg} style={hobbyImageStyle} />
+          </div>
+        </a>
+        <a href="/artMeeting" style={{ textDecoration: "none", color: "#000" }}>
+          <div style={hobbyBoxStyle}>
+            공예
+            <img src={ArtImg} style={hobbyImageStyle} />
+          </div>
+        </a>
+        <a
+          href="/musicMeeting"
+          style={{ textDecoration: "none", color: "#000" }}
+        >
+          <div style={hobbyBoxStyle}>
+            음악
+            <img src={MusicImg} style={hobbyImageStyle} />
+          </div>
+        </a>
+        <a
+          href="/dailyMeeting"
+          style={{ textDecoration: "none", color: "#000" }}
+        >
+          <div style={hobbyBoxStyle}>
+            일상
+            <img src={DailyImg} style={hobbyImageStyle} />
+          </div>
+        </a>
+        <a
+          href="/foodMeeting"
+          style={{ textDecoration: "none", color: "#000" }}
+        >
+          <div style={hobbyBoxStyle}>
+            음식
+            <img src={FoodImg} style={hobbyImageStyle} />
+          </div>
+        </a>
+        <a
+          href="/tripMeeting"
+          style={{ textDecoration: "none", color: "#000" }}
+        >
+          <div style={hobbyBoxStyle}>
+            여행
+            <img src={TripImg} style={hobbyImageStyle} />
+          </div>
+        </a>
+
+        <h4
+          style={{
+            padding: "35px 0px 10px 0px",
+            marginLeft: "27px",
+            textAlign: "left",
+            color: "#787878",
+          }}
+        >
+          취미를 함께 할 사람들을 찾아보세요
+        </h4>
         <div
+          style={{
+            border: "0.8px solid #A5A5A5",
+            width: "350px",
+            height: "144px",
+            borderRadius: "10px",
+            margin: "0px auto",
+          }}
+        >
+          <div
             style={{
-                textAlign: 'center',
-                width: '100%'
-            }}>
-            <div
-                style={{
-                    width: '100%',
-                    margin: '3rem auto'
-                }}>
-                <Search placeholder="찾으시는 취미를 검색해보세요!" onSearch={onSearch} style={{
-                        textAlign: 'center',
-                        width: '90%'
-                    }}
-                    // enterButton
-                    size='large'/>
-
-                <h4
-                    style={{
-                        textAlign: 'left',
-                        color: '#787878',
-                        paddingTop: '30px',
-                        marginLeft: '27px'
-                    }}>취미 카테고리</h4>
-
-                <a href='/exerciseMeeting' style={{textDecoration: 'none', color: '#000'}}>
-                    <div style={hobbyBoxStyle}>
-                        운동
-                        <img src={ExerciseImg} style={hobbyImageStyle}/>
-                    </div>
-                </a>
-                <a href='/artMeeting' style={{textDecoration: 'none', color: '#000'}}>
-                    <div style={hobbyBoxStyle}>
-                        공예
-                        <img src={ArtImg} style={hobbyImageStyle}/>
-                    </div>
-                </a>
-                <a href='/musicMeeting' style={{textDecoration: 'none', color: '#000'}}>
-                    <div style={hobbyBoxStyle}>
-                        음악
-                        <img src={MusicImg} style={hobbyImageStyle}/>
-                    </div>
-                </a>
-                <a href='/dailyMeeting' style={{textDecoration: 'none', color: '#000'}}>
-                    <div style={hobbyBoxStyle}>
-                        일상
-                        <img src={DailyImg} style={hobbyImageStyle}/>
-                    </div>
-                </a>
-                <a href='/foodMeeting' style={{textDecoration: 'none', color: '#000'}}>
-                    <div style={hobbyBoxStyle}>
-                        음식
-                        <img src={FoodImg} style={hobbyImageStyle}/>
-                    </div>
-                </a>
-                <a href='/tripMeeting' style={{textDecoration: 'none', color: '#000'}}>
-                    <div style={hobbyBoxStyle}>
-                        여행
-                        <img src={TripImg} style={hobbyImageStyle}/>
-                    </div>
-                </a>
-
-                <h4
-                    style={{
-                        padding: '35px 0px 10px 0px',
-                        marginLeft: '27px',
-                        textAlign: 'left',
-                        color: '#787878'
-                    }}>취미를 함께 할 사람들을 찾아보세요</h4>
-                <div
-                    style={{
-                        border: '0.8px solid #A5A5A5',
-                        width: '350px',
-                        height: '144px',
-                        borderRadius: '10px',
-                        margin: '0px auto'
-                    }}>
-                    <div
-                        style={{
-                            float: 'left',
-                            display: 'inline-block',
-                            paddingTop: '28px',
-                            paddingLeft: '36px'
-                        }}>
-                        <p
-                            style={{
-                                fontSize: '19px',
-                                color: '#988165'
-                            }}>취미 모임</p>
-                        <Button
-                            href='/makeMeeting'
-                            style={{
-                                width: '128px',
-                                height: '38px',
-                                background: '#D3BA9C',
-                                borderRadius: '19px',
-                                textDecoration: 'none', 
-                                color: '#000'
-                            }}>모임 만들기 ▷</Button>
-                    </div>
-                    <img
-                        src={CharacterImg}
-                        style={{
-                            width: '126px',
-                            height: '131px',
-                            display: 'inline-block'
-                        }}/>
-                </div>
-
-                <h4
-                    style={{
-                        padding: '35px 0px 10px 0px',
-                        marginLeft: '27px',
-                        textAlign: 'left',
-                        color: '#787878'
-                    }}>취미를 기록하고 공유해보세요</h4>
-                <div
-                    style={{
-                        border: '0.8px solid #A5A5A5',
-                        width: '350px',
-                        height: '144px',
-                        borderRadius: '10px',
-                        margin: '0px auto'
-                    }}>
-                    <div
-                        style={{
-                            float: 'left',
-                            display: 'inline-block',
-                            paddingTop: '28px',
-                            paddingLeft: '36px'
-                        }}>
-                        <p
-                            style={{
-                                fontSize: '19px',
-                                color: '#988165'
-                            }}>일상을 기록하는</p>
-                        <Button
-                            href='/hobbyStory'
-                            style={{
-                                width: '128px',
-                                height: '38px',
-                                background: '#D3BA9C',
-                                borderRadius: '19px',
-                                textDecoration: 'none', 
-                                color: '#000'
-                            }}>취미 스토리 ▷</Button>
-                    </div>
-                    <img
-                        src={HobbyStoryImg}
-                        style={{
-                            width: '126px',
-                            height: '131px',
-                            display: 'inline-block'
-                        }}/>
-                </div>
-            </div>
+              float: "left",
+              display: "inline-block",
+              paddingTop: "28px",
+              paddingLeft: "36px",
+            }}
+          >
+            <p
+              style={{
+                fontSize: "19px",
+                color: "#988165",
+              }}
+            >
+              취미 모임
+            </p>
+            <Button
+              href="/makeMeeting"
+              style={{
+                width: "128px",
+                height: "38px",
+                background: "#D3BA9C",
+                borderRadius: "19px",
+                textDecoration: "none",
+                color: "#000",
+              }}
+            >
+              모임 만들기 ▷
+            </Button>
+          </div>
+          <img
+            src={CharacterImg}
+            style={{
+              width: "126px",
+              height: "131px",
+              display: "inline-block",
+            }}
+          />
         </div>
-    )
+
+        <h4
+          style={{
+            padding: "35px 0px 10px 0px",
+            marginLeft: "27px",
+            textAlign: "left",
+            color: "#787878",
+          }}
+        >
+          취미를 기록하고 공유해보세요
+        </h4>
+        <div
+          style={{
+            border: "0.8px solid #A5A5A5",
+            width: "350px",
+            height: "144px",
+            borderRadius: "10px",
+            margin: "0px auto",
+          }}
+        >
+          <div
+            style={{
+              float: "left",
+              display: "inline-block",
+              paddingTop: "28px",
+              paddingLeft: "36px",
+            }}
+          >
+            <p
+              style={{
+                fontSize: "19px",
+                color: "#988165",
+              }}
+            >
+              일상을 기록하는
+            </p>
+            <Button
+              href="/hobbyStory"
+              style={{
+                width: "128px",
+                height: "38px",
+                background: "#D3BA9C",
+                borderRadius: "19px",
+                textDecoration: "none",
+                color: "#000",
+              }}
+            >
+              취미 스토리 ▷
+            </Button>
+          </div>
+          <img
+            src={HobbyStoryImg}
+            style={{
+              width: "126px",
+              height: "131px",
+              display: "inline-block",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
